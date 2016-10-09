76d3be32dc14ef14a42fbe9d7eef24d8ff78e305
Ishraq-Bari <21507301@student.uwa.edu.au>
2016-10-09 11:38:30 +0800

    Added more tests to basic example to ensure grunt file works fine

--- test/example2.js
+++ test/example2.js
@@ -1,8 +1,20 @@
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+    it('Should return 2 when taking the index of 3 in [1,2,3]', function() {
       assert.equal(2, [1,2,3].indexOf(3));
+    done();
+    it('Should return 0 when taking the index of 1 in [1,2,3]', function() {
+      assert.equal(0, [1,2,3].indexOf(1));
+    done();
     });
+  describe('#pop()',function() {
+    it('Should return 3 when popping  [1,2,3]', function() {
+      assert.equal(3, [1,2,3].pop());
+    done();
+    it('Should return 2 when popping  [1,2]', function() {
+      assert.equal(2, [1,2,3].pop());
+    done();
+    })
   });
 });
