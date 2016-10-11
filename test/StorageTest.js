aa64d1ef889a7dd7e07e555030dc70fe59adbcb7
Tahmer Jay <tahmerjay@gmail.com>
2016-10-11 21:02:20 +0800

    linux is case sensitive, fixed file Path

--- test/StorageTest.js
+++ test/StorageTest.js
@@ -1,6 +1,6 @@
 /*global describe, it*/
 
-var storage = require("../lib/storage.js");
+var storage = require("../lib/Storage.js");
 var file = new storage(__dirname+"/testFile.json");
 var expect = require("chai").expect;
 var assert = require("chai").assert;
