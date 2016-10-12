4f4783ea507c903e3aaec50acf320af8beecf9ab
Fraser Bullock <mrfrase3@gmail.com>
2016-10-12 19:29:52 -0400

    Implemented tones of GUI fixes and features, in the head.html file, you can find a structure of the new buttons menu

--- common/js/socks-ninja.js
+++ common/js/socks-ninja.js
@@ -1,10 +1,22 @@
 $(function() {
 
+	var callingTimeout = function(totaltime, lapse, waited){
+    	if(!$('.chat-body-start').data("calling")) return;
+    	if(waited < totaltime){
+        	$(".chat-body-start .progress-bar").width(((waited/totaltime)*100) + "%");
+        	setTimeout(callingTimeout, lapse, totaltime, lapse, waited + lapse);
+        } else {
+        	$('.chat-btn-cancel').click();
+        }
+    }
+
 	$('.chat-btn-start').click(function(){ // add even to the 'request a mentor' button
 		console.log('sending mentor request');
 		socket.emit('ninja.requestMentor');
 		$('.chat-body-request').hide();
 		$('.chat-body-start').show();
+    	$('.chat-body-start').data("calling", true);
+    	callingTimeout(120000, 200, 0);
 	});
 
 	$('.chat-btn-cancel').click(function(){ // add even to the 'leave' button
@@ -12,6 +24,7 @@ $(function() {
 		socket.emit('ninja.cancelRequest');
 		$('.chat-body-request').show();
 		$('.chat-body-start').hide();
+    	$('.chat-body-start').data("calling", false);
 	});
 
 });
